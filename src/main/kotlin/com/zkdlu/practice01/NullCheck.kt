e70b011daf50c688d02ff47c30300344c53ed95b
zkdlu <zkdlu159951@gmail.com>
2022-12-11 20:37:42 +0900

    safe call

--- src/main/kotlin/com/zkdlu/practice01/NullCheck.kt
+++ src/main/kotlin/com/zkdlu/practice01/NullCheck.kt
@@ -2,6 +2,11 @@ package com.zkdlu.practice01
 
 fun main() {
 
+    val str: String? = null
+    println(str?.length)
+
+    val str2: String? = "ABC"
+    println(str2?.length)
 }
 
 fun startsWithA1(str: String?): Boolean {
