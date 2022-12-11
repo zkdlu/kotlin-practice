d326ea61149d4f302b5a18bb817d3d175b681057
zkdlu <zkdlu159951@gmail.com>
2022-12-11 20:39:31 +0900

    null safe kotlin ver

--- src/main/kotlin/com/zkdlu/practice01/NullCheck.kt
+++ src/main/kotlin/com/zkdlu/practice01/NullCheck.kt
@@ -13,22 +13,14 @@ fun main() {
 }
 
 fun startsWithA1(str: String?): Boolean {
-    if (str == null)
-        throw IllegalArgumentException()
-
-    return str.startsWith("A")
+    return str?.startsWith("A")
+        ?: throw IllegalArgumentException()
 }
 
 fun startsWithA2(str: String?): Boolean? {
-    if (str == null)
-        return null
-
-    return str.startsWith("A")
+    return str?.startsWith("A")
 }
 
 fun startsWithA3(str: String?): Boolean {
-    if (str == null)
-        return false
-
-    return str.startsWith("A")
+    return str?.startsWith("A") ?: false
 }
