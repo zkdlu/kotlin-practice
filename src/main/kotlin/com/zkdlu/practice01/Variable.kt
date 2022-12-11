34954128fe2143823e814875890363e4f86d6b72
zkdlu <zkdlu159951@gmail.com>
2022-12-11 20:32:15 +0900

    first

---
+++ src/main/kotlin/com/zkdlu/practice01/Variable.kt
@@ -0,0 +1,17 @@
+package com.zkdlu.practice01
+
+fun main() {
+    println(10)
+
+    val num = 10L
+
+    val num2: Long = 100L
+
+    println(num)
+    println(num2)
+
+    var num3: Long? = null
+    num3 = 1000L
+
+    println(num3)
+}
