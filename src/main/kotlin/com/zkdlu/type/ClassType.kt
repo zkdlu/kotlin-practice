a7bbfa70a524cbf363006805e2cd3893ccddf344
zkdlu <zkdlu159951@gmail.com>
2022-12-11 21:04:57 +0900

    value type, class type

--- src/main/kotlin/com/zkdlu/type/ClassType.kt
+++ src/main/kotlin/com/zkdlu/type/ClassType.kt
@@ -5,6 +5,9 @@ import com.zkdlu.practice01.Person
 fun main() {
     printNameIfPerson2(null)
     printNameIfPerson2(Person("hi"))
+
+    val person = Person("안녕")
+    println("이름은 ${person.name}")
 }
 
 fun printNameIfPerson2(obj: Any?) {
