f93cec863e7b8bac276aa652542ed1eef31b08ff
zkdlu <zkdlu159951@gmail.com>
2022-12-12 18:08:19 +0900

    kotlin lambda

---
+++ src/main/java/com/zkdlu/practice01/Fruit.java
@@ -0,0 +1,19 @@
+package com.zkdlu.practice01;
+
+public class Fruit {
+    private final String name;
+    private final int price;
+
+    public Fruit(final String name, final int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ src/main/kotlin/com/zkdlu/fp/Lambda.kt
@@ -0,0 +1,44 @@
+package com.zkdlu.fp
+
+import com.zkdlu.practice01.Fruit
+
+fun main() {
+    val fruits = listOf(
+        Fruit("사과", 1000),
+        Fruit("사과", 1200),
+        Fruit("사과", 1200),
+        Fruit("사과", 1500),
+        Fruit("바나나", 3000),
+        Fruit("바나나", 3000),
+        Fruit("바나나", 3500),
+        Fruit("바나나", 10000),
+    )
+
+    val isApple = fun(fruit: Fruit): Boolean {
+        return fruit.name == "사과"
+    }
+
+    val isApple2 = { fruit: Fruit -> fruit.name == "사과" }
+
+    isApple(fruits[0])
+    isApple.invoke(fruits[0])
+
+    filterFruits(fruits, isApple)
+    filterFruits(fruits, isApple2)
+    filterFruits(fruits) { fruit: Fruit -> fruit.name == "사과" }
+    filterFruits(fruits) { fruit -> fruit.name == "사과" }
+    filterFruits(fruits) { it.name == "사과" }
+}
+
+private fun filterFruits(
+    fruits: List<Fruit>, filter: (Fruit) -> Boolean
+): List<Fruit> {
+    val results = mutableListOf<Fruit>()
+    for (fruit in fruits) {
+        if (filter(fruit)) {
+            results.add(fruit)
+        }
+    }
+
+    return results
+}
